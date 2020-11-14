253d4a6096494d016d02604c3200d3716e52dea9
Jacob Gadikian <jacobgadikian@gmail.com>
2020-11-15 02:36:04 +0700

    wget progress bar

--- starport/build.sh
+++ starport/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
+wget --progress=bar:force:noscroll http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
 
 docker buildx build --platform linux/arm64 .
 
