b6e6fdac9caa34b2ec7ebc63ba93a7cde8e969db
Jacob Gadikian <jacobgadikian@gmail.com>
2020-11-15 13:48:29 +0700

    fail on errorr: build.sh

--- starport/build.sh
+++ starport/build.sh
@@ -7,6 +7,12 @@
 # Later we can make this work for more devices and platforms with nearly the same technique.
 # Reasonable build targets include: https://archlinuxarm.org/platforms/armv8
 # For example, the Odroid-N2 is the same software-wise as our Router!
+
+# Fail on error
+set -exo pipefail
+
+
+# Get the 64 bit rpi rootfs for Pi 3 and 4
 wget --progress=bar:force:noscroll http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
 
 # Reintroduce later
@@ -24,11 +30,13 @@ docker build --rm --tag toolbox --file toolbox/Dockerfile.root toolbox
 
 # EXTRACT IMAGE
 
+# Make a temporary directory
+mkdir .tmp
 # remove anything in the way of extraction
 docker run --rm --tty --volume $(shell pwd)/./.tmp:/root/./.tmp --workdir /root/./.tmp/.. toolbox rm -rf ./.tmp/result-rootfs
 # save the image to result-rootfs.tar
 docker save --output ./.tmp/result-rootfs.tar starport
 # Extract the image using docker-extract
 docker run --rm --tty --volume $(shell pwd)/./.tmp:/root/./.tmp --workdir /root/./.tmp/.. toolbox /tools/docker-extract --root ./.tmp/result-rootfs  ./.tmp/result-rootfs.tar
-# Set hostname to starpot
+# Set hostname
 bash -c "echo starport > ./.tmp/result-rootfs/etc/hostname"
