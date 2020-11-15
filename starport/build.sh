b5ebd024cb77e24479d924f87b223671dab99ae2
Jacob Gadikian <jacobgadikian@gmail.com>
2020-11-15 11:47:09 +0700

    optomizing

--- starport/build.sh
+++ starport/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Later we can make this work for more devices and platforms with nearly the same technique.
+# Reasonable build targets include: https://archlinuxarm.org/platforms/armv8
 wget --progress=bar:force:noscroll http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
 
-export ROOT_PASSWD=root
+# Reintroduce later
+# export ROOT_PASSWD=root
+# --build-arg ROOT_PASSWD
 
-docker buildx build --build-arg ROOT_PASSWD --platform linux/arm64 .
+
+# Build the image
+docker buildx build --tag starport --platform linux/arm64 .
 
